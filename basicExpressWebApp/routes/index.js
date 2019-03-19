3c2dcefa4e4f3781aaca0babe249588cec442b30
dhruv3 <dhruv.mnit@gmail.com>
2019-03-19 10:17:23 -0500

    checpoint 1 done

--- basicExpressWebApp/routes/index.js
+++ basicExpressWebApp/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router({
+  caseSensitive: true
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'about' });
+});
+
+/* GET About page. */
+router.get('/About', function(req, res, next) {
+  res.render('AboutPage', { title: 'About' });
 });
 
 module.exports = router;
